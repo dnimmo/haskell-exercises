772644e25fd1e0a6a7876170c02771fce8cdc4b6
David Nimmo <dnimmo@gmail.com>
2017-02-09 13:27:19 +0000

    created separate getPlanetOffset function

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -10,19 +10,24 @@ data Planet = Mercury
             | Neptune
 
 
-secondsInYears :: Float -> Float -> Float
-secondsInYears seconds planetOffset =
-  seconds / (31557600 * planetOffset)
+getPlanetOffset :: Planet -> Float
+getPlanetOffset planet =
+  case planet of
+    Mercury -> 0.2408467
+    Venus -> 0.61519726
+    Earth -> 1
+    Mars -> 1.8808158
+    Jupiter -> 11.862615
+    Saturn -> 29.447498
+    Uranus -> 84.016846
+    Neptune -> 164.79132
+
+
+secondsInYears :: Float -> Planet -> Float
+secondsInYears seconds planet =
+  seconds / (31557600 * (getPlanetOffset planet))
 
 
 ageOn :: Planet -> Float -> Float
 ageOn planet seconds =
-  case planet of
-    Mercury -> secondsInYears seconds 0.2408467
-    Venus -> secondsInYears seconds 0.61519726
-    Earth -> secondsInYears seconds 1
-    Mars -> secondsInYears seconds 1.8808158
-    Jupiter -> secondsInYears seconds 11.862615
-    Saturn -> secondsInYears seconds 29.447498
-    Uranus -> secondsInYears seconds 84.016846
-    Neptune -> secondsInYears seconds 164.79132
+  secondsInYears seconds planet
